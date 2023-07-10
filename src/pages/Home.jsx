65eaa87a33ff11015aa5f5530d2dbe5b820679eb
zanetowell <zanetowell@gmail.com>
2023-07-10 18:23:41 -0400

    Redesigned landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,33 @@
 import React from 'react'
 import Moghorn from '../assets/mog_horn.png'
+import MapBackground from '../assets/mapbackground.jpg'
+import Group from '../assets/ffxivgroup.png'
+import Gridania from '../assets/Gridania.jpg'
 
 
 const Home = () => {
   return (
-    <div className='w-full h-full'>
-      <div className='flex justify-between'>
-        {/* Left Text Blurb */}
-        <div className='mx-auto ml-4'>
-          <div className='text-3xl mb-10 '>
+    <div className='w-full h-full mt-10' id='home'>
+      {/* Container */}
+      <div className={`max-w-7xl mx-auto flex-col justify-center items-center px-7 bg-gray-100 rounded-md h-[800px]`}>
+
+        <div className='flex justify-center items-end'>
+          <p className='text-6xl font-cinzel text-yellow-500 font-extrabold text-center mr-6' id='welcome'>
             Welcome to Eorzea Adventurer!
-          </div>
-          <div className='text-xl text-blue-600 w-2/3'>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia, ea alias. Ducimus minima distinctio excepturi odio, debitis eum delectus sed est consequatur deleniti facilis quaerat possimus dignissimos deserunt, magnam nulla?
-          </div>
+          </p>
+          <img src={Moghorn} className='h-[100px]'/>
         </div>
-        {/* Moogie */}
-        <div className='mx-auto w-[40%]'>
-          <img src={Moghorn} />
+
+        <div className='text-center my-8 text-2xl font-bold'>
+          <p>
+            Embark on a thrilling journey through the enchanting realm of Eorzea, where epic tales unfold, brave heroes rise, and treacherous villains scheme. Immerse yourself in a meticulously crafted universe where stunning landscapes, vibrant  cities, and ancient ruins await your exploration. From the bustling streets of Limsa Lominsa to the majestic spires of Ishgard, every corner of Eorzea is teeming with wonders to behold.
+          </p>
+        </div>
+
+        <img src={Group} />
+
+        <div className='text-center my-6 text-2xl font-bold'>
+          Eorzea awaits your arrival, beckoning you to embark on an extraordinary odyssey filled with excitement, danger, and unforgettable memories. Begin your adventure here, and let our website be your guide to the fantastical realm of Final Fantasy XIV's Eorzea!
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'cinzel': ['Cinzel', 'sans-serif'],
+        'merriweather': ['Merriweather', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
