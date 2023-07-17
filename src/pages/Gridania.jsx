597e9b2ba7a69a823b616a397050f614ebb4d2fb
zanetowell <zanetowell@gmail.com>
2023-07-17 16:34:01 -0400

    removes descriptions from landmarks

--- src/pages/Gridania.jsx
+++ src/pages/Gridania.jsx
@@ -7,6 +7,14 @@ const Gridania = () => {
   const [seeDetails, setSeeDetails] = useState()
   const handleClick = () => setSeeContents(!seeContents)
   const handleDetails = () => setSeeDetails(!seeDetails)
+  const reveal = () => {
+    if (className = 'hidden') {
+      className = 'pl-4'
+    } else {
+      className = 'hidden'
+    }
+  }
+
 
   return (
     <div className='w-full h-[200vh] mt-[5%]' id='gridania'>
@@ -46,102 +54,37 @@ const Gridania = () => {
         </div>
 
       </div>
-
-      <div className={`max-w-7xl mx-auto flex justify-center items-center px-7 bg-gray-100 rounded-md pb-12 my-[5%] shadow-md pt-4`}>
-        <div className='flex flex-col ml-4 mt-4'>
+            {/* Landmarks Container */}
+      <div className={`max-w-7xl mx-auto flex justify-start items-center px-7 bg-gray-100 rounded-md pb-12 my-[5%] shadow-md pt-4`}>
+        <div className='ml-4 mt-4 w-full'>
           <h2 className='font-cinzel text-5xl border-b-2 mb-4'>Landmarks </h2>
             <ul className='text-xl list-disc ml-4'>
               <h3 className='text-3xl'>New Gridania</h3>
-              <li onClick={handleDetails} className='cursor-pointer'>Gridania Aetheryte Plaza</li>
-                <span className={!seeDetails 
-                  ? 'hidden'
-                  : 'pl-4'
-                }>A raised platform where the city-state's Aetheryte Crystal is located. Chocobo stables are located between this plaza and the Carline Canopy.</span>
+              <li>Gridania Aetheryte Plaza</li>
               <li>Carline Canopy</li>
-              <span className={!seeDetails 
-                  ? 'hidden'
-                  : 'pl-4'
-                }>A tavern and Adventurer's Guild run by Mother Miounne. Players starting in Gridania will initially be working under her. Inn and Levequest services are also available. Airship docks are located in the back of the establishment.</span>
               <li>Carpenters' Guild</li>
-              <span className={!seeDetails 
-                  ? 'hidden'
-                  : 'pl-4'
-                }>Also known as the Oak Atrium, this guild is responsible for Gridania's timber industry. Carpenters can study their craft in this hall.</span>
               <li>Quiver's Hold</li>
-              <span className={!seeDetails 
-                  ? 'hidden'
-                  : 'pl-4'
-                }>These barracks in an eastern vale serve as the Archers' Guild.</span>
               <li>Adders' Nest</li>
-              <span className={!seeDetails 
-                  ? 'hidden'
-                  : 'pl-4'
-                }>The headquarters of the Order of the Twin Adder. Players who are affiliated with that Grand Company can obtain tasks, goods, and create a Free Company here.</span>
-              <li>Blue Badger Gate</li>
-              <span className={!seeDetails 
-                  ? 'hidden'
-                  : 'pl-4'
-                }>A gate at the south end past the Carline Canopy, this exit leads to the Jadeite Thick in the Central Shroud.</span>
-              <li>White Wolf Gate</li>
-              <span className={!seeDetails 
-                  ? 'hidden'
-                  : 'pl-4'
-                }>A gate at the western end, leading to Sorrel Haven in the Central Shroud. Due the dangerous creatures that live there, access is restricted to travelers who have authorization.</span>
               <h3 className='text-3xl'>Old Gridania</h3>
               <li>Shaded Bower</li>
-              <span className={!seeDetails 
-                  ? 'hidden'
-                  : 'pl-4'
-                }>The main commercial hub of Gridania, several NPC merchants have assorted basic goods. In addition, Market Boards and Retainer Summoning Bells are available for players here.</span>
               <li>Wailing Barracks</li>
-              <span className={!seeDetails 
-                  ? 'hidden'
-                  : 'pl-4'
-                }>These barracks are the home of the Wood Wailers, who operate the Lancers' Guild here.</span>
               <li>Leatherworkers' Guild</li>
-              <span className={!seeDetails 
-                  ? 'hidden'
-                  : 'pl-4'
-                }>Also known as Atelier Fen-Yll, this building serves as a guild for leatherworking.</span>
               <li>Mih Khetto's Amphitheatre</li>
-              <span className={!seeDetails 
-                  ? 'hidden'
-                  : 'pl-4'
-                }>A large area that becomes the venue for many events held in Gridania.</span>
               <li>Botanists' Guild</li>
-              <span className={!seeDetails 
-                  ? 'hidden'
-                  : 'pl-4'
-                }>Located at the western end, this facility is dedicated to botany and horticulture, and owns the Greatloam Growery.</span>
               <li>Nophica's Altar</li>
-              <span className={!seeDetails 
-                  ? 'hidden'
-                  : 'pl-4'
-                }>An altar dedicated to Nophica, the Matron, it serves as the entrance to the Lotus Stand.</span>
               <li>Lotus Stand</li>
-              <span className={!seeDetails 
-                  ? 'hidden'
-                  : 'pl-4'
-                }>Gridania's seat of power, only those with permission may enter. Kan-E-Senna is usually seen here.</span>
               <li>Stillglade Fane</li>
-              <span className={!seeDetails 
-                  ? 'hidden'
-                  : 'pl-4'
-                }>This sanctuary is the home of the Conjurers' Guild, where initiates are trained in communing with the elementals.</span>
               <li>Westshore Pier</li>
-              <span className={!seeDetails 
-                  ? 'hidden'
-                  : 'pl-4'
-                }>Located outside the Wailing Barracks, this pier offers ferry services to the East Shroud.</span>
-              <li>Yellow Serpent Gate</li>
-              <span className={!seeDetails 
-                  ? 'hidden'
-                  : 'pl-4'
-                }>The main point of entry to/from the North Shroud.</span>
             </ul>
         </div>
       </div>
 
+            {/* Nearby Areas */}
+      <div className={`max-w-7xl mx-auto flex justify-start items-center px-7 bg-gray-100 rounded-md pb-12 my-[5%] shadow-md pt-4`}>
+        <div className='ml-4 mt-4 w-full'>
+          <h2 className='font-cinzel text-5xl border-b-2 mb-4'>Surrounding Areas </h2>
+        </div>
+      </div>
 
     </div>
   )
